ad1062f2a8b2d12fdca009f274e390559210f605
ssite1 <pasha.oleksii@elitar.rv.ua>
2019-06-01 15:18:20 +0300

    mysmallsite

---
+++ button.js
@@ -0,0 +1,26 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+function Function() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function move() {
+  var elem = document.getElementById("myBar"); 
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
